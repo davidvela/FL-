4960afa9391ed0cbe048506c66294ffe8758b59b
david <david.vela.tirado@gmail.com>
2018-02-06 00:05:51 +0100

    back up

--- pBk/pDev/tRun.1.py
+++ pBk/pDev/tRun.1.py
@@ -23,11 +23,17 @@ def get_models(type):
 def print_results(execc, typ = "pt"): 
     print("0<60_1>60__0<23_1<60_2<93_3>93  ")
     #open file 
-    file = md.LOGDAT +  "/_logs_tr2.txt" # + md.DESC +
+    # file = md.LOGDAT + md.DESC + "/_logs_tr2.txt"
+    file = md.LOGDAT +  "/_logs_tr2.txt"
     f = open(file, 'w')
+        
     # for i in range(len(md.dsp)): 
-    for i in range(10): #40 
-        # algorithm to check the probs of the pred.        # check concitions - only when diff > 3 
+    for i in range(10): #40
+        # print("m:{0:15} - R-{4:5}   ||_____________C1-{1:2}_____________C2-{2:2}_____________C3-{3}" 
+        # .format(md.dsp.iloc[i,0], execc[0]["pt"][1][i][0], execc[1]["pt"][1][i][0],  execc[2]["pt"][1][i], md.dsp.iloc[i,1], ))    
+        # algorithm to check the probs of the pred.
+        # .. 
+        # check concitions - only when diff > 3 
         gt3 = 0;  gt3, gtM = md.comp_perf(md.dsp.iloc[i,1], execc[2]["pt"][1][i][0]  )
         line = print_line(execc, i, typ)        # if gt3 == 1:  
         print(line )
@@ -56,6 +62,7 @@ def mainRun():
     # DATA READ  ------------------------------------------------ 
     md.mainRead2(ALL_DS, 1, 2 ) # , all = True, shuffle = True  ) 
     # md.mainRead2(path=ALL_DS, part=1, batch_size=2 ) # For testing I am forced to used JSON - column names and order may be different! 
+    
 
     url_test = md.LOGDAT + "FREXP1/" ; # url_test = "url"
     force = False; excel = True  # dataFile = "frall2_json.txt"; labelFile = "datal.csv"     
