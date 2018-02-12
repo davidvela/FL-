0d61ac3a5bf3a6e1a95d9a9b86c51f49ae45a9ca
david <david.vela.tirado@gmail.com>
2018-02-12 16:27:27 +0100

    App and Client connection

--- pDev2/App.py
+++ pDev2/App.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
+from datetime import datetime
+import mRun as mr
+import utils_data as md
+
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -8,17 +12,52 @@ api = Api(app, version='1.0', title='FP API',   description='Prototype to Predic
 
 ns = api.namespace('fp', description='FP Operations')
 
-todo = api.model('fp', {
-    # 'id': fields.Integer(readOnly=True, description='The fp unique identifier'),
-    'form': fields.String(required=True, description='The form details'),
-    'pred': fields.String(required= False,description='Prediction') ) })
+fp = api.model('fp', {
+    #'id': fields.Integer(readOnly=True, description='The fp unique identifier'),
+    'forml': fields.String(required=True, description='The form details'),
+    'pred': fields.String(required= False,description='Prediction') })
+
+def get_models(type):
+    if type == "FRFLO":
+        return [
+            { 'dt':'C2',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 5000, "pe": [], "pt": []  },
+            { 'dt':'C4',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 5000, "pe": [], "pt": []  },
+            { 'dt':'C1',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 5000, "pe": [], "pt": []  },
+        ]
+    elif type == "FRALL1":
+        return [
+            { 'dt':'C2',  "e":40,  "lr":0.001, "h":[100 , 100], "spn": 10000, "pe": [], "pt": []  },
+            { 'dt':'C4',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 10000, "pe": [], "pt": []  },
+            { 'dt':'C1',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 10000, "pe": [], "pt": []  },
+            # { 'dt':'C0',  "e":100, "lr":0.001, "h":[100 , 100], "spn": 10000, "pe": [], "pt": []  },
+        ]
+    else: return []
 
 class fpDAO(object):
     def __init__(self):
         self.counter = 0
         self.todos = []
 
-    def get(self, id):
+    def get(self, forml ):
+        print("___Start!___" +  datetime.now().strftime('%H:%M:%S')  )
+        final = "_" ;  md.DESC = "FRALL1"; # FRAFLO / FRALL1
+        execc = get_models(md.DESC)
+        ex = execc[2]
+        md.spn = ex["spn"]; md.dType = ex["dt"]; mr.epochs = ex["e"]; mr.lr = ex["lr"]; mr.h = ex["h"]
+      
+        mr.ninp = 1814
+        mr.ninp, mr.nout, mr.top_k = md.getnn(mr.ninp)
+        
+        md.MODEL_DIR = md.LOGDIR + md.DESC + '/'   + mr.get_hpar(mr.epochs, final=final) +"/" 
+        mr.model_path = md.MODEL_DIR + "model.ckpt" 
+        mr.build_network3()                                                                                                                                                                                                                                                                                    
+       
+        print(mr.model_path)    
+       
+        ex["pe"]   = mr.evaluate( )
+        ex["pt"] = mr.tests(url_test, p_col=False  )
+
+
         for todo in self.todos:
             if todo['id'] == id:
                 return todo
@@ -33,30 +72,44 @@ class fpDAO(object):
 
     def update(self, id, data):
         return "update"
-        # todo = self.get(id)
-        # todo.update(data)
-        # return todo
 
     def delete(self, id):
         return "delete"
-        # todo = self.get(id)
-        # self.todos.remove(todo)
+
 
 DAO = fpDAO()
 # @ns.route('/')
 # class TodoList(Resource):
 
-@api.route('/<string:todo_id>')
+@api.route('/<string:forml>')
 # api.add_resource(Todo, '/todo/<int:todo_id>', endpoint='todo_ep')
 # @api.route('/todo/<int:todo_id>', endpoint='todo_ep')
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+@ns.param('forml', 'Dummy par - not working!')
+class fpPred(Resource):
+    def get(self, forml):
+        forml2 = '{ "m":"1", "100023" : 1 }'
+        if (len(request.form) > 0):
+            forml2 = request.form['data'];  print(forml2)
+        
+        print(forml) # this is not working! 
+        
+        # pred = DAO.get(forml2)
+        return {"pred": 24}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+    def put(self, forml):
+        # forml = request.form['data']
+        print(forml)
+        return {"pred": 25}
 
         
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+def test_curl():
+    pass
+    # { "m":"PBV10476AS", "178583" :0.74598 , "106104" :0.1 , "182789" :0.04 , "130172" :0.035 , "179661" :0.035 , "164421" :0.018 , "600040" :0.0108 , "116165" :0.008 , "164419" :0.0018 , "103396" :0.001 , "130217" :0.001 , "131460" :0.001 , "690750" :0.0007 , "611089" :0.0006 , "130058" :0.0004 , "130354" :0.0002 , "131101" :0.0002 , "131435" :0.00012 , "131136" :0.0001 , "131315" :0.0001 }   
+    # { "m":"1", "100023" : 1 }
+    # $ curl http://localhost:5000/FORML{213} -d "data=Change my brakepads" -X GET
+    # curl http://localhost:5000/form1Level -d "data='{ "m":"PBV10476AS", "178583" :0.74598 , "106104" :0.1 , "182789" :0.04 , "130172" :0.035 , "179661":0.035 , "164421" :0.018 , "600040" :0.0108 , "116165" :0.008 , "164419" :0.0018 , "103396" :0.001 , "130217" :0.001 , "131460" :0.001 , "690750" :0.0007 , "611089" :0.0006 , "130058" :0.0004 , "130354" :0.0002 , "131101" :0.0002 , "131435" :0.00012 , "131136" :0.0001 , "131315" :0.0001 }'" -X GET
+    #.
